ade4f55d09dd2e1b3ebbcd88e421956a97d48ab3
Svt314 <devilfesh659@gmail.com>
2025-05-28 18:46:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,24 @@
 const CACHE_NAME = 'visiting-card-v1';
-const OFFLINE_URL = 'offline.html';
 const ASSETS = [
   '/',
   '/index.html',
   '/styles.css',
   '/app.js',
   '/avatar.png',
-  '/qr-phone.png',
-  '/qr-telegram.png',
-  '/qr-vk.png',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  OFFLINE_URL
+  '/github-qr.png',
+  '/manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
+self.addEventListener('install', (e) => {
+  e.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
+      .then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
   );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request)
-        .catch(() => caches.match(OFFLINE_URL))
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then((response) => response || fetch(event.request))
-    );
-  }
 });
